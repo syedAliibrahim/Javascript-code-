63b9b8d7e481e3b2bf9c4ffac760af46b325e77a
ali <syedaliibrahim50@gmail.com>
2022-09-07 23:31:31 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+// alert("Welcome to " + cities[3]);
+// var cities = ["kara", "loh", "12345", "true ", "12?"];
+// console.log(cities[1,2]);
+
+// var cities =[];
+// cities.push("kara", "loh");
+// cities.unshift("islam")
+// cities.pop()
+
+
+
+
+
+// console.log(cities)
+// var cities = [ "kar","loh",]
+// var a = cities.shift();
+// console.log(a)
+
+// var food =+prompt ["orange", "yellow", "green" , "blue" ,"black"];
+
---
+++ class9.js
@@ -0,0 +1,485 @@
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = (b-a) * c;
+// alert(d);
+
+// var a= 5;
+// var b =++a;
+// alert(b);
+// var x = 10;
+// var y = x++  + ++x - x  + x++ - ++x + x;
+//      // 10   + 12 - 12  +12   - 14   +14
+// alert (y);
+// var a = 8;
+// var b = 4;
+
+// console.log(a == b);
+// var num = "2" + "2";
+// console.log (num)
+// message = ("Hello," + "Dolly");
+// console.log(message)
+// var saying = "upsy-" + "daisy";
+
+//  console.log(saying)
+// var num = 20 % 6;
+// // console.log(num)
+// var age = +prompt("enter your age");
+// if ( age == 14){
+//     alert("your age is equal")
+// }
+// else if (age < 14){
+//     alert("your age is less")
+// }
+// else{
+//     alert("you are no more ")
+// // }
+// var age = +prompt("enter your age");
+
+// var gender = prompt("enter your gender")
+// if (age > 18 || gender == "male"){
+//     alert("you are allowed to ride")
+// }
+// else{
+//     alert("you are not allowed")
+// }
+// var age = 34 ;
+// var name = "ali" ;
+// var isfree = false;
+
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof isfree);
+
+// var isfree = 25;
+// console.log(typeof isfree)
+
+// var apple = 4 ;
+// // var Apple = 5 ;
+// alert(Apple);
+// var a = 8;
+// var b = 4;
+
+// var c =a +b;
+// var d = a - b;
+// var e = a * b;
+// var f = a / b;
+// var g = a % b;
+// var h = a ** b;
+
+// console.log(c);
+// var a = 5;
+// var b = 4;
+// var c =;
+// alert(*a);
+
+// var name = prompt("what is your age");
+// console.log(name);
+// var a = 5;
+// var b = "hello";
+
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// var a = 15;
+// var b = 6;
+// var c = a < 10 || a > 20 ;
+// // console.log(c);
+// var a = 60;
+// var b = a > 100;
+
+// var c = !b;
+// var score = 68;
+// var a = score > 50 && score < 70 && score == 68;
+
+// console.log(a);
+// var age = 50;
+// if (age > 60){
+//     console.log("you age "+age);
+// }
+// else{
+//     console.log("in else ahe"+age)
+// }
+// var score = 92;
+// if(score > 90){
+//     console.log("Grade A+")
+// }
+// else if (score > 80){
+//     console.log("Grade A")
+// }
+// else if (score > 65){
+//     console.log("Grade B")
+// }
+// else if (score > 50){
+//     console.log("Grade C")
+// }
+// else if (score < 50 ){
+//     console.log("Grade Fail")
+// }
+// var score = 90;
+// if(score > 79){
+//     console.log("Grade A")
+//     if (score >89){
+//         console.log("rewarrd 100$");
+//     }
+//     else if (score >84){
+//         console.log("rewarrd 50$");
+//     }
+//     else{
+//         console.log("rewarrd 20$")
+//     }
+// }
+// else{
+//     console.log("failed")
+// }
+// var a = 1;
+// var b = 8;
+// var c = a && b;
+// console.log("c ="+c);
+
+// for (var i = 5; i <=8; i++){
+//     console.log(i)
+// }
+// console.log(before_break);
+
+// for (var i = 0; i < 5; i++ ){
+//     if (i ==3){
+//         break;
+//     }
+//     console.log(" i = "+i);
+// }
+// console.log(after_continue);
+
+// for (var i = 0; i < 5; i++ ){
+//     if (i ==3){
+//         continue;
+//     }
+//     console.log(" i = "+i);
+// }
+// function great (firstName, lastName){
+//     alert("hello, " + firstName + " "+ lastName );
+// }
+// great("ghous");
+// // great("ahmed");
+// // great("ali");
+// function sum (a,b){
+//     return a+b;
+
+// }
+// var result = sum(2,3);
+// var resultl = sum(10,3);
+
+// function example(num1,num2,opert){
+//     if(opert==="+"){
+//         result88=num1+num2
+//     }
+//     else if(opert==="-"){
+//         result88=num1-num2
+//     }
+//     else if(opert==="*"){
+//         result88=num1*num2
+//     }
+//     else if(opert==="/"){
+//         result88=num1/num2
+//     }
+//     return result;
+// }
+
+// var c =example(66,77,"+")
+// // console.log
+// for (var i =1; i<=8; i++){
+//     console.log(i);
+// }
+// table FOR LOOP
+// var num = +prompt("pleas enter your number");
+// for (var i=1; i<=10; i++ ){
+//     console.log(num+" x " +i+"="+(num*i));
+// }
+// //                 ARRRAY
+// // var students = ["asd" , "ghf", "kloljh"];
+// // // console.log(students[1])
+// // alert("wel"+ students[1])
+
+// var names =[];
+// names[0]="ali";
+// names[1]="hgff";
+
+// console.log(names)
+// //             POP
+// //    Using the keyword, POP , you can remove the last element of an array.
+
+
+// var students = ["abbas" , "ali", "shane"];
+// students.pop() remove end value
+// console.log(students);
+
+// //             PUSH
+// // Using the keyword, push, you can add one or more elements to the end of an array.
+
+
+// var students = ["abbas" , "ali", "shane"];
+
+// students.push("ibrahim");
+// //  push Element add a value to end
+// console.log(students);
+            
+// //                  SHIFT
+//     // remove Element from start 
+// var students = ["abbas" , "ali", "shane"];
+// students.shift()
+// console.log(students);
+
+//         // UNSHIFT
+// //         Add Element at start
+// var students = ["abbas" , "ali", "shane"];
+// students.unshift("bilal" , "zunaib")
+// console.log(students);
+
+//              SPLICE
+
+// var students = ["abbas" , "ali", "shane","abuzar","salman","ibrahim"];
+// students.splice(1,3,"bilal" , "zunaib") 
+//  // first value traget kis ko karna hai,
+// // // second,kahan tak delete karna hai ..
+// // third value ki jaga rakhni hai ye nhi
+// console.log(students);
+// outPut ['abbas', 'bilal', 'zunaib', 'salman', 'ibrahim']
+
+            //    Slice
+            // Use the slice method to copy one or more consecutive elements in any position and put
+            // them into a new array
+// var students = ["abbas" , "ali", "shane", "abuzar" ,"salman","bilal" , "zunaib"];
+//  var copyStudents = students.slice(1,6)
+// console.log(students);
+// console.log(copyStudents)
+// outPut  ['ali', 'shane', 'abuzar', 'salman', 'bilal']
+
+// var foods =["pizza","burger","snacks"];
+// for (var i=0; i<foods.length;i++){
+//     alert(foods[i]);
+// }
+
+//             //    Task Search in Array;
+// var arr = [32,54,55,6,8,2,46,4,5,78,99,88,]
+// var input =+prompt("enter number to find");
+
+// console.log("user input",input)
+// var found = false;
+// for ( i=0; i<arr.length; i++){
+    
+//     if (arr[i] ===input){
+//         console.log("found " +input +" on index = "+i);
+//         found =true;
+//         break;
+//     }
+// }
+// if (!found ){
+//     console.log("not found")
+// }
+
+// var currentTime = 7;
+// console.log(true || false)
+           
+    //                      Logical Operators
+
+    // && . - logical AND | True if both conditions are true
+    // ||  - logical OR | True if any of the condition is true
+    // ! logical NOT| Will return true if the condition is false and vice versa
+
+// var i = 7;
+// i += 1;
+// i += 2;
+// console.log(i)
+// console.log(3 + 4 ** 5)
+
+                    // Changing case 
+
+
+// var city = prompt('enter your city name')
+// var firstchar = city.slice(0,1)
+// var otherchar = city.slice(1)
+// firstchar = firstchar.toUpperCase()
+// otherchar =otherchar.toLocaleLowerCase()
+
+// var city = firstchar +otherchar;
+// console.log(city)
+// var arr =['Karachi','Islamabad','pishawer']
+// for(var i=0; i<arr.length;i++) {
+//     if(arr[i]===city){
+//         alert('city has found')
+//     }
+// }
+
+//     // Head tail
+// var headsuser = prompt("enter head user");
+// var tailssuser = prompt("enter tails user");
+
+// var toss =Math.random()*2;
+// var floor=Math.floor(toss)
+// console.log(floor)
+// if (floor ===0){
+//     alert("heads" + headsuser + "win")
+// }else{
+//     alert("tails" + tailssuser + "win")
+// }
+
+// var num =3.;
+// var cont = num.toFixed(0)
+// console.log(cont)
+// var a = new Date();
+// console.log(a)
+
+// var date = new Date("2020 9 8");
+// console.log(date);
+// var date = new Date();
+// console.log (date);
+
+// console.log(date.getDate());
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
+// console.log(date.getSeconds());
+
+// date.setFullYear(2020);
+// date.setHours(15)
+// console.log(date)
+
+
+                 // function
+
+// function greeting(){
+//     alert("hey ali")
+// }
+// greeting();
+
+
+// console.log("before function")
+// function showMessage(){
+//     console.log("hello world ali ibrahim ");
+// }
+// console.log("after function")
+// showMessage();
+// console.log("after function calling")
+
+
+        //   Parameters Example
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(4,5);
+// sum(6,9)
+// sum(14,5)
+
+
+
+// function calculateScore(sub1,sub2,sub3){
+//     return(sub1+sub2+sub3) / 3;
+
+// }
+// function findGrade(score){
+//     if(score>80 && score <=100){
+//         console.log("Grade A+")
+//     }
+//     else if(score>70 && score <=80){
+//         console.log("Grade b")
+//     }
+//     else if(score>60 && score <=70){
+//         console.log("Grade c")
+//     }
+//     else {
+//         console.log("Grade f")
+//     }
+// }
+// var score1 = calculateScore(77,78,60);
+// var score2 =calculateScore(88,69,88);
+// console.log ("score1 ="+score1);
+// console.log ("score2 ="+score2);
+
+// findGrade(score1);
+// findGrade(score2);
+
+                //  Function in Expressions
+                // function multiply ( num1 , num2 ) {
+                //     return num1 * num2 ;
+                // }
+                // function sum ( a , b ) {
+                // }
+                //     return multiply ( a , b ) + b ;
+                // // var mul = multiply ( 8 , 3 ) ;
+                // // var add = sum ( multiply ( 7 , 3 ) , 5 ) ;
+                // var add = sum ( 7,5 ) ; |
+                // //console.log("Multiply = " , mul ) ;
+                // console.log ( " Addition = " , add ) ;  
+                
+        //         function multiply ( num1 , num2 ) {
+        //             return num1 * num2 ;
+        //         }
+        //         function sum ( a , b ) {
+        //             return a + b ;
+        //         }
+        // // var mul multiply ( 8 , 3 ) ;
+        // var add = sum ( multiply ( 8 , 3 ) , 5 ) ; 
+        // //console.log("Multiply = " , mul ) ;
+        // console.log ( " Addition = " , add ) ;
+                   
+
+        // Local vs Global Variables
+        // var a = 7 ;              // Global Variable
+        // function sum ( ) {
+        //     var b = 6 ;          // Local Variable
+                             
+        //     a = b + 5 ;
+        //     console.log ( " A " + a ) ; // Accessing global variable
+        // }
+        // sum ( ) ;
+        // console.log ( " A = " + a ) ; // 11 , value of a updated
+
+        // example#2
+
+        // var a = 7;   //  Global Variable
+        // function sum ( ) {
+        //   var a = 6 ;  // Local Variable a same name as global
+        //   a = 3 + 2 ;   // Local a variable will be affected
+        //   console.log ( " A  " + a ) ; // 5 , access local variable
+        // }
+        // sum ( ) ;
+        // console.log ( " A " + a ) ; // 7 , access global variable
+
+               
+        // Function Hoisting
+
+// var total =sum (4,3)
+// console.log ("total=" ,total);
+
+// function sum (a, b){
+//     return a+b;
+// }
+
+
+                                // Arguments Passed Value & Reference
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
